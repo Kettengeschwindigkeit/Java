54c358394f8f9304feb244c0cff14d2dd69e1b40
Kettengeschwindigkeit <666damned999@mail.ru>
2023-10-22 19:30:53 +0300

    #1 hello world

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello java");
+    System.out.print("Hello");
+    System.out.print(" World");
+    System.out.print(" hello\n\n\n");
+    System.out.print("\tworld\n");
+    System.out.print("\t\t\t*** My Java first program! ***");
+  }
+}
